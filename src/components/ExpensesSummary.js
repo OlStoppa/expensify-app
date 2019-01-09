72dce4aaac642bc83afec6176b7b17965814edb7
Oliver Lennon <olthespud@hotmail.com>
2019-01-09 12:15:19 +0800

    tackled styling

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,8 +12,13 @@ export const ExpensesSummary = (props) => {
 	const expense = length === 1 ? 'expense' : 'expenses';
 	const total = numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00');
 	return(
-		<div>
-			<h1>{`Viewing ${length} ${expense} totalling ${total}`}</h1>
+		<div className="page-header">
+			<div className="content-container">
+			<h1 className="page-header__title">Viewing <span>{length}</span> {expense} totalling <span>{total}</span></h1>
+			<div className="page-header__actions">
+				<Link className="button" to="/create">Add Expense</Link>
+			</div>
+			</div>
 			
 		</div>
 
