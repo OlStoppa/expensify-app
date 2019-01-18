7c96d9f761149fba3ec4fd7875446adae126af81
OlStoppa <olthespud@hotmail.com>
2019-01-18 13:04:27 +0800

    styled lists and inputs

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -41,13 +41,17 @@ export class ExpenseListFilters extends React.Component {
 			<div className="content-container">
 				<div className="input-group">
 					<div className="input-group__item">
-						<input type="text" 
+						<input 
+							type="text"
+							className="text-input" 
+							placeholder="search expenses"
 							value={this.props.filters.text} 
 							onChange={this.onTextChange}
 						/>
 					</div>
 					<div className="input-group__item">
 						<select 
+							className="select"
 							value={this.props.filters.sortBy} 
 							onChange={this.onSortChange}>
 							<option value="date">Date</option>
